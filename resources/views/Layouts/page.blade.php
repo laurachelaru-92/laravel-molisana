f0d5dc64341834784616e788c70bb4122bc39ff1
Laura <laura.a.chelaru@gmail.com>
2020-10-20 19:04:54 +0200

    footer completato

--- resources/views/Layouts/page.blade.php
+++ resources/views/Layouts/page.blade.php
@@ -36,7 +36,32 @@
         </div>
         <div id="right-foot">
           <ul>
-
+            <h3>Pastificio</h3>
+            <li><a href="#">Il Pastificio</a></li>
+            <li><a href="#">Grano decorticato a pietra</a></li>
+            <li><a href="#">Il Molise c'è</a></li>
+            <li><a href="#">Filiera Integrata</a></li>
+            <li><a href="#">100 anni di pasta</a></li>
+            <li><a href="#">Sartoria della pasta</a></li>
+            <li><a href="#">Spaghetto Quadrato</a></li>
+            <li><a href="#">Le Persone</a></li>
+          </ul>
+          <ul>
+            <h3>Collezione da chef</h3>
+            <li><a href="#">Collezione da Chef</a></li>
+            <li><a href="#">Grandi Cucine</a></li>
+            <li><a href="#">Biologiche</a></li>
+            <li><a href="#">Quadrate</a></li>
+          </ul>
+          <ul>
+            <h3>Prodotti</h3>
+            <li><a href="#">Le Classiche</a></li>
+            <li><a href="#">Le Integrali</a></li>
+            <li><a href="#">Le Speciali</a></li>
+            <li><a href="#">Le Biologiche</a></li>
+            <li><a href="#">Le Gluten-Free</a></li>
+            <li><a href="#">Le Semole</a></li>
+            <li><a href="#">Le Extra di Lusso</a></li>
           </ul>
         </div>
       </div>
