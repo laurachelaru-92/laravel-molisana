054091876ee320a4beaeb21f2a9342c42b471115
Laura <laura.a.chelaru@gmail.com>
2020-10-22 16:03:00 +0200

    modificato img in apici singoli in prodotti 6 e 7

--- resources/views/Partial_pages/prodotto_singolo.blade.php
+++ resources/views/Partial_pages/prodotto_singolo.blade.php
@@ -8,9 +8,9 @@
 <main class="no-bg">
     <div class="container" id="product">
         <h1>{{$pasta["titolo"]}}</h1>
-        <img src="{{$pasta["src-h"]}}" alt="">
-        <img src="{{$pasta["src-p"]}}" alt="">
-        <p>{!!$pasta["descrizione"]!!}</p>
+        <img src='{{$pasta["src-h"]}}' alt="">
+        <img src='{{$pasta["src-p"]}}' alt="">
+        <p>{!!$pasta['descrizione']!!}</p>
     </div>
 
     @if($id > 0)
